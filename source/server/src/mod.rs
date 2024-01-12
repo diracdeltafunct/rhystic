950d076d02a2c832ae9e562d0491b8fda720c365
Matt Muckle <matt.muckle@brightspec.com>
2024-01-11 22:43:37 -0500

    running dev server

--- source/server/src/mod.rs
+++ source/server/src/mod.rs
@@ -1,12 +1,1 @@
-use axum::{Router, routing::get};
-
-
-
-pub async fn start_dev_server(){
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+pub mod server;
